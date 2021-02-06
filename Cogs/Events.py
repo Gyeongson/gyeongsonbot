de8c70fd22d524a6f514a3defd362a9ab3a2c984
Chip <chip1020@kakao.com>
2021-02-06 10:14:17 +0900

    Change & Fixed

--- Cogs/Events.py
+++ Cogs/Events.py
@@ -33,46 +33,47 @@ class CommandsEvent(commands.Cog):
             return
 
         elif isinstance(error, discord.Forbidden):
-            await ctx.send(f"{ctx.author.mention} 권한 부족 이유로 명령어 실행에 실패했어요.")
+            await ctx.send(f"봇이 권한이 부족해요.\n\nDM 등 개인에게 전송해야 하는 커맨드를 사용하신 경우 DM 차단 여부를 확인해주세요.")
 
         elif isinstance(error, commands.CommandNotFound):
-            await ctx.send("해당 명령어를 찾을 수 없어요. `경손아 도움` 으로 명령어를 확인해보세요.")
+            await ctx.send("알 수 없는 커맨드 에요, `경손아 도움` 에서 찾아보세요.")
 
         elif isinstance(error, commands.MissingPermissions):
-            mp = error.missing_perms
-            p = perms[mp[0]]
-            await ctx.send(f"{ctx.author.mention} 이 명령어를 실행하기 위해서는 다음과 같은 권한이 필요합니다. : `{p}`")
+            missingpermission = error.missing_perms
+            permission_korean = perms[missingpermission[0]]
+            await ctx.send(f"이 명령어를 실행하기 위해서는 다음 권한이 필요해요. : `{permission_korean}`")
 
         elif isinstance(error, commands.BotMissingPermissions):
-            mp = error.missing_perms
-            p = perms[mp[0]]
-            await ctx.send(f"{ctx.author.mention} 이 명령어를 실행하기 위해서는 봇에게 다음과 같은 권한이 필요합니다. : `{p}`")
+            missingpermission = error.missing_perms
+            permission_korean = perms[missingpermission[0]]
+            await ctx.send(f"이 명령어를 실행하기 위해서 봇에게 다음 권한이 필요해요. : `{permission_korean}`")
 
         elif isinstance(error, commands.MissingRequiredArgument) or isinstance(error, commands.BadArgument):
             if isinstance(error, commands.MemberNotFound) or isinstance(error, commands.UserNotFound):
-                await ctx.send(f"{ctx.author.mention} 해당 유저를 찾을 수 없어요, 유저를 다시 확인해주세요.")
+                await ctx.send(f"알 수 없는 유저에요.")
 
             elif isinstance(error, commands.ChannelNotFound):
-                await ctx.send(f"{ctx.author.mention} 채널을 찾을 수 없었어요, 채널을 다시 확인해주세요.")
+                await ctx.send(f"알 수 없는 채널이에요.")
 
             elif isinstance(error, commands.ChannelNotReadable):
-                await ctx.send(f"{ctx.author.mention} 해당 채널을 봇이 읽거나 볼 수 없어요, 권한을 다시 확인해주세요.")
+                await ctx.send(f"채널을 봇이 읽거나 볼 수 없어요.")
 
             elif isinstance(error, commands.RoleNotFound):
-                await ctx.send(f"{ctx.author.mention} 역할을 찾을 수 없었어요, 역할을 다시 확인해주세요.")
+                await ctx.send(f"역할을 찾을 수 없었어요.")
             
             elif isinstance(error, commands.NotOwner):
-                await ctx.send("해당 명령어는 개발자 전용 커맨드에요.")
+                await ctx.send("명령어가 개발자 전용 커맨드에요.")
             
             elif isinstance(error, commands.MissingRole):
-                await ctx.send(f"해당 명령어는 특정 역할 전용 커맨드에요.")
+                await ctx.send(f"명령어가 특정 역할 전용 커맨드에요.")
             else:
                 usage = ctx.command.help.split("\n")[0]
-                await ctx.send(f"{ctx.author.mention} `{usage}`(이)가 올바른 명령어에요!")
+                await ctx.send(f"`{usage}` 가 올바른 사용방법 이에요.")
         else:
             channel = self.bot.get_channel(config.errorterminal)
-            await channel.send(f"```예기치 못한 오류가 발생했어요. {ctx.command.name} : {error}```")
-            await ctx.send(f"{ctx.author.mention} 명령어 실행 도중 오류가 발생했어요.\n\n이 오류가 지속될 경우 Discord 지원 서버로 문의해주세요. https://discord.gg/4uwv3UVEwv")
+            await channel.send(f"```예기치 못한 오류가 발생했어요.\n\n오류가 발생한 명령어: {ctx.command.name}\n오류: {error}```")
+            embed = discord.Embed(title="예기치 못한 오류가 발생했어요.", description="명령어를 실행하던중 오류가 발생해 사용이 취소되었어요.\n\n[이곳](https://discord.gg/4uwv3UVEwv) 에서 도움을 요청할 수 있어요.", color=0x428bff)
+            await ctx.send(embed=embed)
     
 def setup(bot):
     bot.add_cog(CommandsEvent(bot))
--- main.py
+++ main.py
@@ -26,22 +26,4 @@ bot.load_extension('Cogs.Events')
 bot.load_extension('Cogs.General')
 bot.load_extension('Cogs.Moderation')
 
-@bot.event
-async def on_message(message):
-    if message.channel.type == discord.ChannelType.private:
-        return
-
-    if message.author.bot:
-        return
-
-    if message.content.startswith("경손아 "):
-        if "'" in message.content or '"' in message.content or "\\" in message.content or ";" in message.content:
-            channel = bot.get_channel(config.terminal)
-            await channel.send(f"```Command Denied : {message.author} ( {message.author.id} ) - {message.content} / Guild : {message.guild.name} ( {message.guild.id} )```")
-            await message.channel.send(f"{message.author.mention} 특수문자를 제외하고 다시 시도해주세요.")
-        else:
-            channel = bot.get_channel(config.terminal)
-            await channel.send(f"```Command Processed : {message.author} ( {message.author.id} ) - {message.content} / Guild : {message.guild.name} ( {message.guild.id} )```")
-            await bot.process_commands(message)
-
 bot.run(config.token)
